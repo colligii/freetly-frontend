98ea36a22f54af3413e78795262e31705b6b651b
Giovanne Colli <cgiovanne45@gmail.com>
2026-08-23 19:07:48 -0300

    install radix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './router.tsx'
 import './main.css'
+import { Theme } from '@radix-ui/themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <Theme>
+      <Router />
+    </Theme>
   </StrictMode>,
 )
