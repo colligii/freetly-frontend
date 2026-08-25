50d9d7b53ac92811ffb599456b1894065f5d73c6
Giovanne Colli <cgiovanne45@gmail.com>
2026-08-25 09:19:06 -0300

    remove unused import

--- src/routes/demo/router.tsx
+++ src/routes/demo/router.tsx
@@ -1,6 +1,5 @@
 import LandingPage from "./landing-page";
 import TestEnviroment from "../../components/testenv";
-import { Component } from "react";
 import DemoSuccess from "./success";
 
 export const demoChildren = [
